6f3440f0f1e8a39e9f8227d61310696d18ff591c
gongsua <thddbswls11@naver.com>
2025-10-15 23:45:03 +0900

    init: Buying Shop Spring Boot project

---
+++ src/main/java/org/example/buyingserver/BuyingServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.buyingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/buyingserver/BuyingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.buyingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
