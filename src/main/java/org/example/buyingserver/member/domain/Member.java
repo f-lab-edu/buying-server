89423f84579b82f033c27692258303e06738c3ff
gongsua <thddbswls11@naver.com>
2025-10-20 21:55:39 +0900

    null 제약 추가

--- src/main/java/org/example/buyingserver/member/domain/Member.java
+++ src/main/java/org/example/buyingserver/member/domain/Member.java
@@ -1,6 +1,5 @@
 package org.example.buyingserver.member.domain;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +15,9 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @Builder
+@Table(name = "member")
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,42 +25,36 @@ public class Member {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = true)
     private String password;
 
+    @Column(nullable = false)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
     @Builder.Default
+    @Column(nullable = false)
     private Status status = Status.RUNNING;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
     private SocialType socialType;
 
+    @Column(nullable = true)
     private String socialid;
 
     @CreationTimestamp
-    @Column(updatable = false)
+    @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
-
-    private LocalDateTime deletedAt; //소프트 delete 시점
+    @Column(nullable = true)
+    private LocalDateTime deletedAt;
 
     public void markAsDeleted() {
         this.deletedAt = LocalDateTime.now();
         this.status = Status.DELETED;
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
--- src/main/java/org/example/buyingserver/member/domain/Status.java
+++ src/main/java/org/example/buyingserver/member/domain/Status.java
@@ -2,5 +2,4 @@ package org.example.buyingserver.member.domain;
 
 public enum Status {
     RUNNING, DELETED
-
 }
