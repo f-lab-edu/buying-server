d6327824fe7af18b4e4455d45b3a807d2b229b31
gongsua <thddbswls11@naver.com>
2025-12-25 15:22:56 +0900

    refactor : PostRepository에 비관적 락 메서드 추가

--- src/main/java/org/example/buyingserver/post/repository/PostRepository.java
+++ src/main/java/org/example/buyingserver/post/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package org.example.buyingserver.post.repository;
 
+import org.springframework.data.repository.query.Param;
+import jakarta.persistence.LockModeType;
 import org.example.buyingserver.post.domain.Post;
 import org.example.buyingserver.post.domain.PostStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +17,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByStatusIn(List<PostStatus> statuses);
     boolean existsByIdAndStatusNot(Long id, PostStatus status);
     Optional<Post> findById(Long id);
+
+    //비관적 락으로 Post 조회하도록 설정
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Post p where p.id= :id")
+    Optional<Post> findByIdWithLock(@Param("id") Long id);
 }
